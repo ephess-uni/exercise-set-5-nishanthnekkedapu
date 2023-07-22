e9978e9263e721857613e840bcbaa5e42ad9f482
nishanthnekkedapu <136869627+nishanthnekkedapu@users.noreply.github.com>
2023-07-21 22:26:02 -0500

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -11,15 +11,9 @@ import argparse
 if __name__ == "__main__":
 
     ob = ArgumentParser(description="This program applies a standard scale transform to the data in infile and writes it to outfile.")
-    
     ob.add_argument("infile", type=argparse.FileType('r'))
-    
     ob.add_argument("outfile", type=argparse.FileType('w'))
-    root_dir = get_repository_root()
-    INFILE = root_dir / "data" / "ex_5_2-data.csv"
-    OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
     infileData = np.loadtxt("input_data.txt")
-    os.makedirs(root_dir / "outputs", exist_ok=True)
     mn = np.mean(infileData)
     mn_0 = infileData - mn
     std_mean = np.std(mn_0)
