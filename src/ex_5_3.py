ad75b0f33947f8f6ea426487ada18d880df68b59
nishanthnekkedapu <136869627+nishanthnekkedapu@users.noreply.github.com>
2023-07-21 22:21:31 -0500

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -15,8 +15,11 @@ if __name__ == "__main__":
     ob.add_argument("infile", type=argparse.FileType('r'))
     
     ob.add_argument("outfile", type=argparse.FileType('w'))
-    
-    infileData = np.loadtxt(INFILE)
+    root_dir = get_repository_root()
+    INFILE = root_dir / "data" / "ex_5_2-data.csv"
+    OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
+    infileData = np.loadtxt("input_data.txt")
+    os.makedirs(root_dir / "outputs", exist_ok=True)
     mn = np.mean(infileData)
     mn_0 = infileData - mn
     std_mean = np.std(mn_0)
